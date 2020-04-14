3be5fe0e8eeeaf479d744c62f77054fb9c26f10b
Toby Clemson <tobyclemson@gmail.com>
2020-04-14 23:50:53 +0100

    Add setup scripts to docker image.

--- src/openvpn-aws/docker-entrypoint.sh
+++ src/openvpn-aws/docker-entrypoint.sh
@@ -3,4 +3,22 @@
 [ "$TRACE" = "yes" ] && set -x
 set -e
 
-exec "$@"
+# Route IPs through the VPN
+iptables -t nat -A POSTROUTING -s "${VPN_REMOTE_CIDR}" -o eth0 -j MASQUERADE
+
+# Create tunnel device
+mkdir -p /dev/net
+if [ ! -c /dev/net/tun ]; then
+    mknod /dev/net/tun c 10 200
+fi
+
+# Render server config template
+echo "Rendering open server config file."
+envsubst \
+  < /etc/openvpn/server.conf.template \
+  > /etc/openvpn/server.conf
+
+echo "Running openvpn"
+cd /etc/openvpn
+exec openvpn \
+    --config /etc/openvpn/server.conf
