c3ea289a3af436ba337a1ce62efeb3bce2a66fd5
Toby Clemson <tobyclemson@gmail.com>
2020-12-21 02:42:08 +0000

    Fix environment variable in fetch-pki script.

--- src/openvpn-aws/scripts/fetch-pki.sh
+++ src/openvpn-aws/scripts/fetch-pki.sh
@@ -4,23 +4,23 @@ set -e
 set -o pipefail
 
 fetch_file_from_s3 \
-    "${S3_BUCKET_REGION}" \
+    "${AWS_S3_BUCKET_REGION}" \
     "${OPENVPN_CA_CERT_FILE_S3_OBJECT_PATH}" \
     /etc/openvpn/pki/ca.crt
 fetch_file_from_s3 \
-    "${S3_BUCKET_REGION}" \
+    "${AWS_S3_BUCKET_REGION}" \
     "${OPENVPN_SERVER_CERT_FILE_S3_OBJECT_PATH}" \
     /etc/openvpn/pki/server.crt
 fetch_file_from_s3 \
-    "${S3_BUCKET_REGION}" \
+    "${AWS_S3_BUCKET_REGION}" \
     "${OPENVPN_SERVER_KEY_FILE_S3_OBJECT_PATH}" \
     /etc/openvpn/pki/server.key
 fetch_file_from_s3 \
-    "${S3_BUCKET_REGION}" \
+    "${AWS_S3_BUCKET_REGION}" \
     "${OPENVPN_DH_PARAMS_FILE_S3_OBJECT_PATH}" \
     /etc/openvpn/pki/dh2048.pem
 fetch_file_from_s3 \
-    "${S3_BUCKET_REGION}" \
+    "${AWS_S3_BUCKET_REGION}" \
     "${OPENVPN_CRL_FILE_S3_OBJECT_PATH}" \
     /etc/openvpn/pki/crl.pem
 
